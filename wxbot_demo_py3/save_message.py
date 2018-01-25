1d230d5fee8ce4d36f4f9f1b8853434100d928ee
jiyang.li <jiyang.li@timanetworks.com>
2018-01-25 23:11:34 +0800

    init

---
+++ wxbot_demo_py3/save_message.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pymysql
+
+
+"""
+jiyang.li create at 2018-01-25
+保存历史聊天记录:
+    文字类型的聊天记录保存到mysql
+    图片视频的聊天记录保存到硬盘(后期会保存到文件服务器)
+
+"""
+class MysqlDao(object):
+    def __init__(self):
+        self.host = 'localhost'
+        self.user = 'root'
+        self.passwd = 'root'
+        self.db = 'vdr'
+        self.port = '3306'
+        self.conn = None
+
+    def openConn(self):
+        self.conn = pymysql.connect(host="localhost", user="root", password="1234", db="vdr", port=3306)
+
+    def closeConn(self):
+        self.conn.close()
+
+dao = MysqlDao()
+dao.openConn()
+dao.closeConn()
+
